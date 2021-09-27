27339cf40041fe0dd8d3637732f065b914b06e48
John <jddhalogamer@gmail.com>
2021-09-27 14:13:34 -0500

    refactored Content module to import a Part module to use repeatedly with new props for each instance.

--- half-stack/src/components/Content.js
+++ half-stack/src/components/Content.js
@@ -1,9 +1,11 @@
+import Part from './Part.js';
+
 const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
     return (
     <div>
-        <p>Lesson : {part1} / Exercises : {exercises1}</p>
-        <p>Lesson : {part2} / Exercises : {exercises2} </p>
-        <p>Lesson : {part3} / Exercises : {exercises3} </p>
+        <Part part={part1} exercises={exercises1} />
+        <Part part={part2} exercises={exercises2} />
+        <Part part={part3} exercises={exercises3} />
     </div>
     )
 }
---
+++ half-stack/src/components/Part.js
@@ -0,0 +1,9 @@
+const Part = ({part, exercises}) => {
+    return (
+        <div>
+            <p>Lesson : {part} / Exercises : {exercises} </p>
+        </div>
+    )
+}
+
+export default Part;
