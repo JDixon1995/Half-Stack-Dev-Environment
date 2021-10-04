59d8c509ecda6161745142a83252f0363160d5e2
John <jddhalogamer@gmail.com>
2021-10-04 14:12:40 -0500

    removed stateful incrementing counter for a counter that uses a button to increment, and one to reset.

--- half-stack/src/App.js
+++ half-stack/src/App.js
@@ -8,6 +8,10 @@ const App = () => {
 
   const [ counter, setCounter ] = useState(0);
 
+  const increaseByOne = () => setCounter(counter + 1);
+
+  const setToZero = () => setCounter(0)
+
   const course = 'Half stack application development';
   const part1 = 'Fundamentals of React';
   const exercises1 = 10;
@@ -21,15 +25,17 @@ const App = () => {
     age: 26
   }
 
-  setTimeout(
-    () => setCounter( counter + 1 ),
-    1000
-  )
 
   return (
     <div>
       <Hello object={object} />
       <div>{counter}</div>
+      <button onClick={increaseByOne}>
+      +
+      </button>
+      <button onClick={setToZero}>
+        0
+      </button>
       <Header course={course}/>
       <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} /> 
